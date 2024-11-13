0757759ba01da5df4990290672ea0804e14ab979
adventuresoul <vinayme873@gmail.com>
2024-11-13 18:34:01 +0530

    Added basic structure for react project

---
+++ frontend/BlockMeds-frontend/src/pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,9 @@
+function ErrorPage() {
+    return(
+        <div>
+            <h1>This is error page</h1>
+        </div>
+    )
+}
+
+export default ErrorPage;
---
+++ frontend/BlockMeds-frontend/src/pages/SignupPage/SignupPage.jsx
@@ -0,0 +1,9 @@
+function SignupPage() {
+    return(
+        <div>
+            <h1>This is Signup page</h1>
+        </div>
+    )
+}
+
+export default SignupPage;

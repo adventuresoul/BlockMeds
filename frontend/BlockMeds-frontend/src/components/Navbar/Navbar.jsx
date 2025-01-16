b1e20fcad6a8168e95b5acc6c8bff953d7af0705
adventuresoul <vinayme873@gmail.com>
2025-01-16 23:48:39 +0530

    Removed frontend from git

--- frontend/BlockMeds-frontend/src/components/Navbar/Navbar.jsx
+++
@@ -1,9 +0,0 @@
-function Navbar() {
-    return(
-        <div>
-            <h1>This is Navbar</h1>
-        </div>
-    )
-}
-
-export default Navbar;
--- frontend/BlockMeds-frontend/src/pages/ErrorPage/ErrorPage.jsx
+++
@@ -1,9 +0,0 @@
-function ErrorPage() {
-    return(
-        <div>
-            <h1>This is error page</h1>
-        </div>
-    )
-}
-
-export default ErrorPage;
